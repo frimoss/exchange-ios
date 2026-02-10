45559688bcabc6b5ccd08a9c66da77421156eb14
Nikolai <frimoss@yandex.ru>
2026-02-10 13:03:24 +0200

    Create CurrencyListViewController

---
+++ exchange/Module/View/CurrencySheetView/CurrencyListViewController.swift
@@ -0,0 +1,90 @@
+//
+//  CurrencyListViewController.swift
+//  exchange
+//
+//  Created by Nikolai on 10/02/2026.
+//
+
+import UIKit
+
+final class CurrencyListViewController: UIViewController {
+    
+    // Mock Data
+    private let currencies = ["ARS", "EURc", "COP", "MXN", "BRL", "ARS", "EURc", "COP", "MXN", "BRL", "ARS", "EURc", "COP", "MXN", "BRL"]
+    
+    // MARK: - UI Components
+    
+    private var selectedIndex: Int? = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(CurrencyCell.self, forCellReuseIdentifier: CurrencyCell.identifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = .clear
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        view.layer.cornerRadius = 16
+        view.clipsToBounds = true
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 4),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - UITableView Delegate & DataSource -
+
+extension CurrencyListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: CurrencyCell.identifier,
+            for: indexPath
+        ) as? CurrencyCell else {
+            return UITableViewCell()
+        }
+        
+        let currency = currencies[indexPath.row]
+        let isSelected = selectedIndex == indexPath.row
+        
+        cell.configure(currency: currency, isSelected: isSelected)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        tableView.reloadData()
+        
+        let selectedCurrency = currencies[indexPath.row]
+        print("Selected: \(selectedCurrency)")
+    }
+}
---
+++ exchange/Module/View/CurrencySheetView/Part/CurrencyCell.swift
@@ -0,0 +1,54 @@
+//
+//  CurrencyCell.swift
+//  exchange
+//
+//  Created by Nikolai on 10/02/2026.
+//
+
+import UIKit
+
+final class CurrencyCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    static let identifier = "CurrencyCell"
+    
+    // MARK: - UI Components
+    
+    private let currencyItemView = CurrencyItemView()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupView() {
+        contentView.addSubview(currencyItemView)
+        currencyItemView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            currencyItemView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            currencyItemView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            currencyItemView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            currencyItemView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        selectionStyle = .none
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+    }
+    
+    // MARK: - Configuration
+    
+    func configure(currency: String, isSelected: Bool) {
+        currencyItemView.configure(currency: currency, isSelected: isSelected)
+    }
+}
