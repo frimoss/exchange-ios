8ac5d74fcd9144b118978755630c26a8bf17ee79
Nikolai <frimoss@yandex.ru>
2026-02-20 20:49:38 +0200

    Update MockNetworkClient

--- exchange/Network/MockNetworkClient.swift
+++ exchange/Network/MockNetworkClient.swift
@@ -9,22 +9,29 @@ import Foundation
 
 final class MockNetworkClient: NetworkClientProtocol {
     
-    private let filename: String
-    
-    init(filename: String) {
-        self.filename = filename
-    }
-    
     // MARK: - Mock Request Method
     
-    func request<T: Decodable>(_ urlString: String) async throws -> T {
+    func request<T: Decodable>(_ endpoint: TickerEndpoint) async throws -> T {
         
-        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
-            throw NetworkError.invalidURL
+        let filename = mockFilename(for: endpoint)
+        
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else { throw NetworkError.invalidURL
         }
         
         let data = try Data(contentsOf: url)
         
+        print("Using Mock NetworkClient for path: \(endpoint.path) (\(filename).json)")
+        
         return try JSONDecoder().decode(T.self, from: data)
+    }
+    
+    // MARK: - Filenames of Mock JSON Files
+    
+    private func mockFilename(for endpoint: TickerEndpoint) -> String {
+        
+        switch endpoint {
+        case .currencies:   return "currencies"
+        case .tickers:      return "exchange_rate"
+        }
     }
 }
