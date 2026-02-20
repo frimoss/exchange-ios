dd4ef4c293d5c6afbf812a68804371b5ce4b8198
Nikolai <frimoss@yandex.ru>
2026-02-20 10:30:50 +0200

    Update ExchangeViewState

--- exchange/Common/ViewState/ExchangeViewState.swift
+++ exchange/Common/ViewState/ExchangeViewState.swift
@@ -7,13 +7,61 @@
 
 import Foundation
 
+enum ActiveField {
+    case top
+    case bottom
+}
+
+enum ConversionDirection {
+    case usdToSelected // USD to MXN
+    case selectedToUsd // MXN to USD
+}
+
 struct ExchangeViewState {
     
-    var currencies: [Currency] = []
+    // MARK: - Store Data
+    
+    var currencies: [Currency] = [] // Sheet Available Currencies
+    var rates: [String: Decimal] = [:] // All Available Rates ["MXN": 17.17, "ARS": 1466.49]
 
-    var selectedCurrencyCode: String?
+    // MARK: - Selected Currency
     
-    var inputAmount: Decimal = 0
+    var selectedCurrency: Currency = .mxn // USD to MXN (by default)
     
-    var rates: [String: Decimal] = [:]
+    // MARK: - Input Fields
+    
+    var topAmount = "1" // $1 (by default)
+    var bottomAmount = ""
+    
+    var activeField: ActiveField = .top
+    
+    // MARK: - Conversion Direction
+    
+    var direction: ConversionDirection = .usdToSelected // Change by SWAP Button
+    
+    // MARK: - State
+    
+    var isLoading: Bool = false
+    var errorMessage: String?
+    
+    // MARK: - Computed Properties
+    
+    var topCurrency: Currency {
+        switch direction {
+        case .usdToSelected: return Currency.usd
+        case .selectedToUsd: return selectedCurrency
+        }
+    }
+    
+    var bottomCurrency: Currency {
+        switch direction {
+        case .usdToSelected: return selectedCurrency
+        case .selectedToUsd: return Currency.usd
+        }
+    }
+    
+    // Current Rate
+    var exchangeRate: Decimal? {
+        return rates[selectedCurrency.code]
+    }
 }
