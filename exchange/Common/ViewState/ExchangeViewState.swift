8665205b4226a357cc6aea52d37ee7a35c51249f
Nikolai <frimoss@yandex.ru>
2026-02-26 10:04:42 +0200

    Refactoring ExchangeViewState

--- exchange/Common/ViewState/ExchangeViewState.swift
+++ exchange/Common/ViewState/ExchangeViewState.swift
@@ -9,33 +9,11 @@ import Foundation
 
 struct ExchangeViewState {
     
-    // MARK: - Enums
-    
-    enum Status {
-        case isLoading
-        case error(String)
-        case loaded([Currency])
-    }
-
-    enum ActiveField {
-        case top
-        case bottom
-    }
-
-    enum ConversionDirection {
-        case usdToSelected // USD to MXN
-        case selectedToUsd // MXN to USD
-    }
-    
-    // MARK: - Status
-    
-    var status: Status = .isLoading
-    
     // MARK: - Data
     
     var currencies: [Currency] = [] // Sheet Available Currencies
     var rates: [String: Decimal] = [:] // All Available Rates ["MXN": 17.17, "ARS": 1466.49]
-
+    
     // MARK: - Selection
     
     var selectedCurrency: Currency = .mxn // USD to MXN (by default)
@@ -46,6 +24,10 @@ struct ExchangeViewState {
     var topAmount = "1" // $1 (by default)
     var bottomAmount = ""
     var activeField: ActiveField = .top
+    
+    // MARK: - Status
+    
+    var status: Status = .isLoading
     
     // MARK: - Computed Properties
     
@@ -66,5 +48,26 @@ struct ExchangeViewState {
     // Current Rate
     var exchangeRate: Decimal? {
         return rates[selectedCurrency.code]
+    }
+}
+
+// MARK: - Extension ExchangeViewState: Enums -
+
+extension ExchangeViewState {
+    
+    enum Status {
+        case isLoading
+        case error(String)
+        case loaded([Currency])
+    }
+
+    enum ActiveField {
+        case top
+        case bottom
+    }
+
+    enum ConversionDirection {
+        case usdToSelected // USD to MXN
+        case selectedToUsd // MXN to USD
     }
 }
