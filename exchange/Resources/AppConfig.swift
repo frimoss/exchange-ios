b3f6f7edd47215d0a4e36fc86584110363714bc7
Nikolai <frimoss@yandex.ru>
2026-04-06 12:10:14 +0300

    App Config: Add Amount Limits

--- exchange/Resources/AppConfig.swift
+++ exchange/Resources/AppConfig.swift
@@ -8,7 +8,20 @@
 import Foundation
 
 enum AppConfig {
+    
     enum API {
         static let baseURL = "https://api.dolarapp.dev/v1"
+    }
+    
+    /// Amount Limits
+    enum Amount {
+        static let maxDigitsBeforeSeparator = 7         // 1_000_000
+        static let defaultMaxFractionDigits = 2         // 0.12
+        static let highPrecisionMaxFractionDigits = 6   // 0.000992
+        
+        /// Maximum Fraction Digits
+        static func precision(for value: Decimal) -> Int {
+            return (value > 0 && value < 1.0) ? highPrecisionMaxFractionDigits : defaultMaxFractionDigits
+        }
     }
 }
